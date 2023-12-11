a7fc4a4f3dcc0f76b77482cef8737a29b2a7635c
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2023-12-12 00:01:41 +0300

    Authentication with JWT updated.

--- backend/src/main/java/com/tfunsal/ecommerce/filters/JwtRequestFilter.java
+++ backend/src/main/java/com/tfunsal/ecommerce/filters/JwtRequestFilter.java
@@ -38,11 +38,10 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null){
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-            if (jwtUtil.validateToken(token , userDetails)){
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails , null);
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
+            if(jwtUtil.validateToken(token, userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
         filterChain.doFilter(request , response);
