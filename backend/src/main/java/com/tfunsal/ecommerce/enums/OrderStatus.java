fac9044507137401753864706d9de959f9713294
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2023-12-13 22:22:50 +0300

    order and cart items entities created

---
+++ backend/src/main/java/com/tfunsal/ecommerce/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.tfunsal.ecommerce.enums;
+
+public enum OrderStatus {
+
+    Pending ,
+    Placed,
+    Shipped,
+    Delivered,
+}
--- backend/src/main/java/com/tfunsal/ecommerce/services/auth/AuthServiceImpl.java
+++ backend/src/main/java/com/tfunsal/ecommerce/services/auth/AuthServiceImpl.java
@@ -2,8 +2,11 @@ package com.tfunsal.ecommerce.services.auth;
 
 import com.tfunsal.ecommerce.dto.SignupRequest;
 import com.tfunsal.ecommerce.dto.UserDto;
+import com.tfunsal.ecommerce.entity.Order;
 import com.tfunsal.ecommerce.entity.User;
+import com.tfunsal.ecommerce.enums.OrderStatus;
 import com.tfunsal.ecommerce.enums.UserRole;
+import com.tfunsal.ecommerce.repository.OrderRepository;
 import com.tfunsal.ecommerce.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +20,11 @@ public class AuthServiceImpl implements AuthService{
     private UserRepository userRepository;
 
     @Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
 
     public UserDto create(SignupRequest signupRequest){
         User user  = new User();
@@ -27,6 +34,15 @@ public class AuthServiceImpl implements AuthService{
         user.setPassword(new BCryptPasswordEncoder().encode(signupRequest.getPassword()));
         user.setRole(UserRole.CUSTOMER);
         User createdUser = userRepository.save(user);
+
+        Order order = new Order();
+        order.setAmount(0L);
+        order.setTotalAmount(0L);
+        order.setDiscount(0L);
+        order.setUser(createdUser);
+        order.setOrderStatus(OrderStatus.Pending);
+        orderRepository.save(order);
+
 
         UserDto userDto = new UserDto();
         userDto.setId(createdUser.getId());
